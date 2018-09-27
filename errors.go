47b9d920e9f4a2675236f040d8d1edbfd2f5a4a9
Shahzil Abid <shahzilabid@Shahzils-MacBook-Pro.local>
2018-09-27 15:33:44 -0700

    Change password errs

--- errors.go
+++ errors.go
@@ -9,6 +9,6 @@ var ErrNotFound = errors.New("The specified data wasn't found.")
 // ErrLoggedOut is returned if the request responds with a 400 status code
 var ErrLoggedOut = errors.New("The account is logged out")
 
-var ErrBadPassword = errors.New("The password provided is incorrect")
+var ErrBadPassword = errors.New("The Instagram password you provided is incorrect")
 
-var ErrChallenge = errors.New("Challenge required by Instagram")
+var ErrChallenge = errors.New("Instagram needs you to authorize our system logging into your account. Please check your email for a link we have sent you or login to your instagram app and click “This is me”")
