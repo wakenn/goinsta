55b8c87109510f8f4886e479ee8bdc3f046ff8d8
Shahzil Abid <shahzilabid@Shahzils-MacBook-Pro.local>
2018-10-19 12:14:32 -0700

    Remove print

--- request.go
+++ request.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/url"
 	"strings"
@@ -114,7 +113,7 @@ func (insta *Instagram) sendRequest(o *reqOptions) (body []byte, err error) {
 				e = ErrChallenge
 			} else {
 				e = ErrLoggedOut
-				log.Println("Logged out!", string(body))
+				// log.Println("Logged out!", string(body))
 			}
 		case 404:
 			e = ErrNotFound
