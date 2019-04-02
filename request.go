651c9e261ca1cdef9817df53b285441ba701fe97
Shahzil Abid <shahzilabid@Shahzils-MacBook-Pro.local>
2019-04-02 16:27:35 -0700

    Stop

--- request.go
+++ request.go
@@ -103,11 +103,11 @@ func (insta *Instagram) sendRequest(o *reqOptions) (body []byte, err error) {
 		}
 	}
 
-	if insta.Proxy != "" {
-		if sz := respSize(resp); sz > 10000 {
-			log.Println("Large hit:", GOINSTA_API_URL+o.Endpoint, sz)
-		}
-	}
+	// if insta.Proxy != "" {
+	// 	if sz := respSize(resp); sz > 10000 {
+	// 		log.Println("Large hit:", GOINSTA_API_URL+o.Endpoint, sz)
+	// 	}
+	// }
 
 	body, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
